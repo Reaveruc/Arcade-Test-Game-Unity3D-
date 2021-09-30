2f7d9ff213cfbb4d4ec04d8fa87b8466608c0914
Ivan <58460160+Reaveruc@users.noreply.github.com>
2021-09-30 18:27:52 +0700

    Add files via upload

---
+++ Arcade Test Game (Unity3D)/Assets/Scripts/Game/ScrollBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = -2f;
+    [SerializeField]
+    private float lowerYValue = -20f;
+    [SerializeField]
+    private float upperYValue = 40;
+
+    private void ScrolBackGround()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+        if (transform.position.y <= lowerYValue)
+        {
+            transform.Translate(0f, upperYValue, 0f);
+        }
+    }
+    void Update()
+    {
+        ScrolBackGround();
+    }
+
+}
